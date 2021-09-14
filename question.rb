dcd7c02550b7a8f201d9de1bc7e1209f98a85a60
Anton <golubin1990@gmail.com>
2021-09-14 18:35:20 -0400

    Small formatting changes

--- question.rb
+++ question.rb
@@ -6,17 +6,17 @@ class Question
     number_1 = rand(1..20)
     number_2 = rand(1..20)
     answer = number_1 + number_2
-    puts "What does #{number_1} + #{number_2} equal?"
+    puts "#{player.name}: What does #{number_1} + #{number_2} equal?"
     player_answer = $stdin.gets.chomp
     self.check_answer(player_answer, answer)
   end
 
   def check_answer(player_answer, answer)
-    if (player_answer != answer)
-      puts "Seriously? No!"
+    if (player_answer.to_i != answer.to_i)
+      puts "#{self.player.name} Seriously? No!"
       self.player.lost?()
     else 
-      puts "Yes! You are correct!"
+      puts "#{self.player.name} Yes! You are correct!"
     end
   end
 
